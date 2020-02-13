d3b799c926ce4dd3496d29f60e47105ebe3e63be
Vinicius Scot <viniciusdss96@gmail.com>
2020-02-13 10:27:50 -0300

    Adicionar e remover movimentação / Refetch

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Rest from '../utils/rest'
 
 const baseUrl = `https://mymoney-6a4a5.firebaseio.com/`,
-    { useGet } = Rest(baseUrl)
+    { useGet, usePost, useDelete } = Rest(baseUrl)
 
 const Movimentacoes = ({ match }) => {
     const data = useGet(`movimentacoes/${match.params.data}`)
+    const salvar = usePost(`movimentacoes/${match.params.data}`)[1]
+    const remover = useDelete()[1]
+    const [descricao, setDescricao] = useState('')
+    const [valor, setValor] = useState(0)
+
+    const salvarMovimentacao = async () => {
+        await salvar({
+            descricao,
+            valor
+        })
+
+        setDescricao('')
+        setValor(0.0)
+        data.refetch()
+    }
+
+    const removerMovimentacao = async id => {
+        await remover(`movimentacoes/${match.params.data}/${id}`)
+        data.refetch()
+    }
 
     return (
         <div className='container'>
@@ -23,9 +43,19 @@ const Movimentacoes = ({ match }) => {
                     {data.data && Object.keys(data.data).map(movimentacao => (
                         <tr key={movimentacao}>
                             <td>{data.data[movimentacao].descricao}</td>
-                            <td>{data.data[movimentacao].valor}</td>
+                            <td>
+                                {data.data[movimentacao].valor}
+                                <button onClick={() => removerMovimentacao(movimentacao)}>-</button>
+                            </td>
                         </tr>
                     ))}
+                    <tr>
+                        <td><input type='text' value={descricao} id='descricao' onChange={e => setDescricao(e.target.value)} /></td>
+                        <td>
+                            <input type='text' value={valor} id='valor' onChange={e => setValor(e.target.value)} />
+                            <button onClick={salvarMovimentacao}>+</button>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
--- src/utils/rest.js
+++ src/utils/rest.js
@@ -1,4 +1,4 @@
-import { useReducer, useEffect } from 'react'
+import { useReducer, useEffect, useCallback } from 'react'
 import axios from 'axios'
 
 const INITIAL_STATE = {
@@ -21,27 +21,32 @@ const init = baseUrl => {
     const useGet = resource => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
 
-        useEffect(() => {
+        const carregar = useCallback(async () => {
             dispatch({ type: 'REQUEST' })
 
-            axios.get(baseUrl + resource + `.json`).then(res => {
-                dispatch({ type: 'SUCCESS', data: res.data })
-            })
+            const res = await axios.get(baseUrl + resource + `.json`)
+
+            dispatch({ type: 'SUCCESS', data: res.data })
         }, [resource])
 
-        return data
+        useEffect(() => {
+            carregar()
+        }, [carregar])
+
+
+
+        return { ...data, refetch: carregar }
     }
 
     const usePost = resource => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
 
-        const post = data => {
+        const post = async data => {
             dispatch({ type: 'REQUEST' })
 
-            axios.post(baseUrl + resource + `.json`, data).then(res => {
-                dispatch({ type: 'SUCCESS', data: res.data })
-                console.log(res.data)
-            })
+            const res = await axios.post(baseUrl + resource + `.json`, data)
+
+            dispatch({ type: 'SUCCESS', data: res.data })
         }
 
         return [data, post]
@@ -50,12 +55,12 @@ const init = baseUrl => {
     const useDelete = () => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
 
-        const remove = resource => {
+        const remove = async resource => {
             dispatch({ type: 'REQUEST' })
 
-            axios.delete(baseUrl + resource + `.json`).then(() => {
-                dispatch({ type: 'SUCCESS' })
-            })
+            await axios.delete(baseUrl + resource + `.json`)
+
+            dispatch({ type: 'SUCCESS' })
         }
 
         return [data, remove]
